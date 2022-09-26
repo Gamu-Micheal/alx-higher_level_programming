b9fdaf7fc9095c2d26985e7a61e4a74185d1721e
Gamu Micheal <gamumicheal@gmail.com>
2022-09-26 12:04:37 -0700

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -17,4 +17,4 @@ class Square(Rectangle):
     def __init__(self, size):
         """ instantiation with size """
         self.__size = size
-        super().__init__(self.__size, self.__size) 
+        super().__init__(self.__size, self.__size)
