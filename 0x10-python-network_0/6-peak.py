c4f74e40f3a9f5bfb09b6332c49a763d37bf7e8d
Gamu Micheal <gamumicheal@gmail.com>
2022-12-16 10:22:46 -0800

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+"""
+Function that finds a peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    """
+    finds num that's greater than both left and right
+    """
+    if len(list_of_integers) == 0:
+        return None
+
+    lists = list_of_integers
+    beg = 0
+    end = len(lists)-1
+
+    if lists[beg] > lists[beg+1]:
+        return lists[beg]
+    if lists[end] > lists[end-1]:
+        return lists[end]
+
+    mid = (beg+end)//2
+    if lists[mid-1] < lists[mid] and lists[mid+1] < lists[mid]:
+        return lists[mid]
+    if lists[mid] < lists[mid-1]:
+        return find_peak(lists[beg:mid+1])
+    elif lists[mid] < lists[mid+1]:
+        return find_peak(lists[mid:end+1])
+    else:
+        return lists[beg]
